6825a4a3d3efc559802d49c82bbd5a81ca5681bd
selvaganeshr <selvaganeshrajam@gmail.com>
2017-01-29 12:39:03 +0530

    index updated

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
+var connections = [];
+var users = [];
+
 
 server.listen(process.env.PORT || 3000);
 
@@ -9,4 +12,19 @@ console.log('server running on port 3000');
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
+});
+
+
+io.sockets.on('connection', function(socket) {
+    connections.push(socket);
+    console.log('Connected: %s sockets connected', connections.length);
+
+    socket.on('disconnect', function(data) {
+        connections.splice(connections.indexOf(socket), 1);
+        console.log('Disconnected: %s sockets connected', connections.length);
+    });
+
+    socket.on('send message', function(data) {
+        io.sockets.emit('new message', { msg: data });
+    });
 });
